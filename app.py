992b25ec9422d3a45837009da306ac8be2e16760
Daniyar Akparov <daniyarkorp@gmail.com>
2023-10-01 23:04:57 +0300

    add adding

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 
+# читаем goods.txt
 def homepage():
     f = open('goods.txt','r', encoding='utf-8') 
     txt = f.readlines() 
     return render_template('index.html', goods = txt)
+
+# записываем через форму добавления в goods.txt
+@app.route('/add/', methods=['POST'])
+def add():
+    good = request.form['good']
+    f = open('goods.txt','a+', encoding='utf-8') 
+    f.write(good + '\n')
+    f.close()
+    return """
+        <h1>Инвентарь пополнен<h1>
+        <a href = '/'> Домой <a>
+        """
